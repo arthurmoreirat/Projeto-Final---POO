8bea946a96e0a667dc3f0aa7a4ca84c24e53f9da
Arthur Moreira <arthur27.mtenorio@gmail.com>
2025-10-04 23:11:12 -0300

    feat: Adicionar interface usada pelas classes

---
+++ src/Interface.java
@@ -0,0 +1,7 @@
+public interface Interface {
+    public int curar(int cura);
+    public int getVidaMaxima();
+    public int usarHabilidade();
+    public int atacar(int defesa);
+    public void receberDano(int dano);
+}
